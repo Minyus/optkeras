0a6caaf6428e11ce64be04b6ae3818d85ad3126e
Minyus <yusuke.minami86@gmail.com>
2019-02-15 17:04:55 +0800

    added Google-style docstrings

--- optkeras/optkeras.py
+++ optkeras/optkeras.py
@@ -8,20 +8,47 @@ import optuna
 
 
 class OptKeras(Callback):
+    """ The main class of OptKeras which can act as a callback for Keras.
+    """
     def __init__(self, 
-                 monitor = 'val_error', # alternatively 'val_loss'
+                 monitor = 'val_error',
                  enable_pruning = False,
                  enable_keras_log = True,
                  keras_log_file_suffix = '_Keras.csv',
                  enable_optuna_log = True,
                  optuna_log_file_suffix = '_Optuna.csv',
-                 models_to_keep = 1, # either 1, 0, or -1 (save all models)
+                 models_to_keep = 1,
                  model_file_prefix = 'model_', 
                  model_file_suffix = '.hdf5',
                  directory_path = '',
                  verbose = 1,
-                 **kwargs):                     
-        # Create Optuna Study
+                 **kwargs):
+        """ Wrapper of optuna.create_study
+        Args:
+            monitor: The metric to optimize by Optuna. 'val_error' in default or 'val_loss'.
+            enable_pruning: Enable pruning by Optuna.
+                See https://optuna.readthedocs.io/en/latest/tutorial/pruning.html
+            enable_keras_log: Enable logging by Keras CSVLogger callback.
+                See https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/CSVLogger
+            keras_log_file_suffix: Suffix of the file if enable_keras_log is True.
+                '_Keras.csv' in default.
+            enable_optuna_log: Enable generating a log file by Optuna study.trials_dataframe().
+                See https://optuna.readthedocs.io/en/latest/reference/study.html
+            optuna_log_file_suffix: Suffix of the file if enable_optuna_log is True.
+            models_to_keep: The number of models to keep.
+                either 1 in default , 0, or -1 (save all models).
+            model_file_prefix: Prefix of the model file path if models_to_keep is not 0.
+                'model_' in default.
+            model_file_suffix: Suffix of the model file path if models_to_keep is not 0.
+                '.hdf5' in default.
+            directory_path: The path of the directory for the files.
+                '' (Current working directory) in default.
+            verbose: How much to print messages onto the screen.
+                0 (no messages), 1 in default, 2 (troubleshooting)
+            **kwargs: parameters for optuna.study.Study():
+                study_name, storage, sampler=None, pruner=None, direction='minimize'
+                See https://optuna.readthedocs.io/en/latest/reference/study.html
+        """
         self.study = optuna.create_study(**kwargs)
         self.study_name = self.study.study_name
         self.keras_log_file_path = directory_path + self.study_name + keras_log_file_suffix
@@ -47,15 +74,33 @@ class OptKeras(Callback):
             'Ready for optimization. (message printed as verbose is set to 1+)')
 
     def optimize(self, *args, **kwargs):
+        """
+        Args:
+            *args: parameters for study.optimize optimize(): func
+            **kwargs: parameters for study.optimize optimize():
+                n_trials=None, timeout=None, n_jobs=1, catch=(<class 'Exception'>, )
+                See https://optuna.readthedocs.io/en/latest/reference/study.html
+
+        Returns: None
+        """
         self.study.optimize(*args, **kwargs)
         self.post_process()
 
     def get_model_file_path(self, trial_id = None):
+        """
+        Args:
+            trial_id: Trial Id of Optuna study
+
+        Returns: model file path string
+        """
         return ''.join([self.model_file_prefix, 
                         '*' if trial_id is None else '{:06d}'.format(trial_id),
                         self.model_file_suffix ])
 
-    def clean_up_model_files(self): 
+    def clean_up_model_files(self):
+        """ Delete model files not needed
+        Returns: None
+        """
         # currently version supports only models_to_keep <= 1
         if self.models_to_keep in [1]:
             self.best_model_file_path = \
@@ -67,9 +112,16 @@ class OptKeras(Callback):
                     os.remove(model_file)
 
     def get_datetime(self):
+        """
+        Returns: date time string in '%Y-%m-%d %H:%M:%S.%f' format
+        """
         return datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
 
     def callbacks(self, trial):
+        """ Callbacks to be passed to Keras
+        Args:
+            trial: Optuna trial
+        """
         self.synch_with_optuna()
         callbacks = []
         self.trial = trial
@@ -92,10 +144,16 @@ class OptKeras(Callback):
         return callbacks
 
     def save_logs_as_optuna_attributes(self):
+        """ Save the logs in Optuna's database as user attributes.
+        Returns: None
+        """
         for key, val in self.trial_best_logs.items():
             self.trial.set_user_attr(key, val)
 
     def generate_optuna_log_file(self):
+        """ Generate log file from Optuna
+        Returns: None
+        """
         try:
             df = self.study.trials_dataframe()
             df.columns = ['_'.join(str_list(col)).rstrip('_').
@@ -108,6 +166,9 @@ class OptKeras(Callback):
             'Failed to generate Optuna log file. Continue to run.')
 
     def synch_with_optuna(self):
+        """ Exchange information with Optuna
+        Returns: None
+        """
         # Generate the Optuna CSV log file
         if self.enable_optuna_log: self.generate_optuna_log_file()
         # best_trial
@@ -128,6 +189,9 @@ class OptKeras(Callback):
         if self.verbose >= 1: self.print_results()
 
     def print_results(self):
+        """ Print summary of results
+        Returns: None
+        """
         if self.verbose >= 1 and len(self.study.trials) > 0:
             # if any trial with a valid value is found, show the result
             print(
@@ -140,15 +204,28 @@ class OptKeras(Callback):
                 ', parameters: {}'.format(self.best_trial.params) )
 
     def post_process(self):
+        """ Process after optimization
+        Returns: None
+        """
         self.synch_with_optuna()
         self.clean_up_model_files()
 
     def on_epoch_begin(self, epoch, logs={}):
+        """ Called at the beginning of every epoch by Keras
+        Args:
+            epoch:
+            logs:
+        """
         self.datetime_epoch_begin = self.get_datetime()
         ## Reset trial best logs
         self.trial_best_logs = {}
 
     def on_epoch_end(self, epoch, logs={}):
+        """ called at the end of every epoch by Keras
+        Args:
+            epoch:
+            logs:
+        """
         self.datetime_epoch_end = self.get_datetime()
         # Add error and val_error to logs for use as an objective to minimize
         logs['error'] = 1 - logs.get('acc', 0)
@@ -182,4 +259,9 @@ class OptKeras(Callback):
 
 
 def str_list(input_list):
+    """ Convert all the elements in a list to str
+    Args:
+        input_list: list of any elements
+    Returns: list of string elements
+    """
     return ['{}'.format(e) for e in input_list] # convert all elements to string
