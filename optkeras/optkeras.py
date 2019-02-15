31c9bbaa1e5417e574a357999c036ece7b0b6656
Minyus <yusuke.minami86@gmail.com>
2019-02-15 17:18:18 +0800

    revised docstring about optuna.study.create_study()

--- optkeras/optkeras.py
+++ optkeras/optkeras.py
@@ -45,7 +45,7 @@ class OptKeras(Callback):
                 '' (Current working directory) in default.
             verbose: How much to print messages onto the screen.
                 0 (no messages), 1 in default, 2 (troubleshooting)
-            **kwargs: parameters for optuna.study.Study():
+            **kwargs: parameters for optuna.study.create_study():
                 study_name, storage, sampler=None, pruner=None, direction='minimize'
                 See https://optuna.readthedocs.io/en/latest/reference/study.html
         """
